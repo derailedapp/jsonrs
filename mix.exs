fa0c44a33e61e123191a60c6c481a17d61d346a7
VincentRPS <vincentbusiness55@gmail.com>
2024-12-03 17:03:14 +0800

    chore: remove rustler precompiled
    Signed-off-by: VincentRPS <vincentbusiness55@gmail.com>

--- mix.exs
+++ mix.exs
@@ -27,7 +27,6 @@ defmodule Jsonrs.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler_precompiled, "~> 0.8.0"},
       {:rustler, "~> 0.35.0", optional: true, runtime: false},
       {:ex_doc, "~> 0.30", only: :dev, runtime: false},
     ]
